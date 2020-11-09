c9404e6ec33f16f186041d7809e11e1f5dad3998
Muhammad Ihya Firdaus B <raito12cs@gmail.com>
2020-11-09 21:37:38 +0700

    add navbar

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -19,24 +19,38 @@
 </head>
 
 <body class="bg-gray-100">
-    <nav>
-        <a href="">Home</a>
-        <a href="">Data Postingan</a>
-        <a href="">Data Ibu</a>
-        <a href="">Data Anak</a>
-        <a href="">Laporan</a>
-        <a href="">Tracking</a>
-        <a href="{{ route('logout') }}" onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">Logout</a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
+    <nav class="navigation-bar">
+        <div class="navigation-logo">
+            Logo Sigenting
+        </div>
+        <div class="navigation-links">
+            <a href="" class="navigation-link">Home</a>
+            <a href="" class="navigation-link">Data Postingan</a>
+            <a href="" class="navigation-link">Data Ibu</a>
+            <div class="dropdown">
+                <a href="#" class="dropdown-trigger">
+                    Data Anak
+                    <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                </a>
+                <div class="dropdown-content duration-500">
+                    <a href="" class="navigation-link">List Data Anak</a>
+                    <a href="" class="navigation-link">Laporan</a>
+                    <a href="" class="navigation-link">Tracking</a>
+                </div>
+            </div>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();" class="navigation-link">Logout</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
     </nav>
 
     @yield('content')
 
-    <script src="https://code.jquery.com/jquery-2.2.4.js"
-        integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js" defer></script>
 
     @yield('script')
--- resources/views/livewire/ibu.blade.php
+++ resources/views/livewire/ibu.blade.php
@@ -10,7 +10,34 @@
 
     @livewireStyles
 </head>
-
+<nav class="navigation-bar">
+        <div class="navigation-logo">
+            Logo Sigenting
+        </div>
+        <div class="navigation-links">
+            <a href="" class="navigation-link">Home</a>
+            <a href="" class="navigation-link">Data Postingan</a>
+            <a href="" class="navigation-link">Data Ibu</a>
+            <div class="dropdown">
+                <a href="#" class="dropdown-trigger">
+                    Data Anak
+                    <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                </a>
+                <div class="dropdown-content duration-500">
+                    <a href="" class="navigation-link">List Data Anak</a>
+                    <a href="" class="navigation-link">Laporan</a>
+                    <a href="" class="navigation-link">Tracking</a>
+                </div>
+            </div>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();" class="navigation-link">Logout</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+    </nav>
 <body class="bg-gray-100">
     <div class="container my-5">
         <div class="row justify-content-center">
